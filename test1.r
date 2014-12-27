96225a9ff1c3c2cd3ff3f90d43565777566b76f4
Bob Riley <coachriley1@gmail.com>
2014-12-27 09:24:00 -0800

    Changed code to complete assignment.  Did not annotate code.

--- test1.r
+++ test1.r
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-##  This function creates a special "matrix" object that can cache its inverse.
-
 makeCacheMatrix <- function(x = numeric()) {
         inv <- NULL
         set <- function(y) {
@@ -17,14 +12,7 @@ makeCacheMatrix <- function(x = numeric()) {
              getsolve = getsolve)
 }
 
-
-## This function computes the inverse of the special "matrix" returned by 
-## makeCacheMatrix above. If the inverse has already been calculated (and the matrix 
-## has not changed), then the cachesolve should retrieve the inverse from the cache.
-
 cacheSolve <- function(x, ...) {
-
-        ## Return a matrix that is the inverse of 'x'
         inv <- x$getsolve()
         if(!is.null(inv)) {
                 message("getting cached data")
